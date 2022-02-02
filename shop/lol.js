44ab6887cd9a94751d5380f1bf4e03c83f36a3f5
NekoKyun <91820453+Yuri-Neko@users.noreply.github.com>
2022-02-02 21:21:45 +0700

    Update lol.js

--- shop/lol.js
+++ shop/lol.js
@@ -1,19 +1,19 @@
 const list_lol = (prefix) => {
-return `*ZEEONE OFC さ*
+return `*Kyoukaさ*
 				
 PROSES	1-180Mnt 1X24 Jam			
-OPEN	10.00-19.00			
+OPEN				
 FORMAT	ID			
 				
 WC LOLM				
 			
-125WC	Rp 18,000.00			
-420WC	Rp 55,000.00			
-700WC	Rp 85,000.00			
-1.375WC	Rp 155,000.00			
-2.400WC	Rp 250,000.00			
-4,000.00WC	Rp 395,000.00			
-8.150WC	Rp 785,000.00			
+125WC				
+420WC				
+700WC			
+1.375WC				
+2.400WC			
+4,000.00WC				
+8.150WC			
 				
 Untuk pay / pembayan silahkan ketik ${prefix}pay
 `
